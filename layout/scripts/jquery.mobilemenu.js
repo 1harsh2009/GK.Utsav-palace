f68265cc6e30d1d2cab4c3376716302afe678f89
1harsh2009 <deekshasanghmitra1108@gmail.com>
2023-12-12 21:10:28 +0530

    vfdsa

--- layout/scripts/jquery.mobilemenu.js
+++ layout/scripts/jquery.mobilemenu.js
@@ -13,3 +13,15 @@
 // */
 
 // $('<form action="#"><select /></form>').appendTo("#mainav");$("<option />",{selected:"selected",value:"",text:"MENU"}).appendTo("#mainav select");$("#mainav a").each(function(){var e=$(this);if($(e).parents("ul ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"- - - "+e.text()}).appendTo("#mainav select")}else if($(e).parents("ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"- - "+e.text()}).appendTo("#mainav select")}else if($(e).parents("ul").length>=1){$("<option />",{value:e.attr("href"),text:""+e.text()}).appendTo("#mainav select")}else{$("<option />",{value:e.attr("href"),text:e.text()}).appendTo("#mainav select")}});$("#mainav select").change(function(){if($(this).find("option:selected").val()!=="#"){window.location=$(this).find("option:selected").val()}})
+
+
+
+a = document.querySelectorAll(".one_third")
+console.log(a);
+if (window.innerWidth <500+"px"){
+    a.classList.add("one_half")
+    a.classList.remove("one_third")
+}
+else{
+    
+}
