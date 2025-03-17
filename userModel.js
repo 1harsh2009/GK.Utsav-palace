c2733492d50febf1f9bd01ec2afc14b430992030
1harsh2009 <150052287+1harsh2009@users.noreply.github.com>
2025-03-17 22:07:05 +0530

    Update userModel.js

--- userModel.js
+++ userModel.js
@@ -14,6 +14,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    date:{
+        type:
+    },
     Reviwed:{
         type:String,
         default: 'No'
